cffa0645c9dc6317e773c13d4b429b6967f1a837
Your Name <you@example.com>
2021-12-14 20:44:38 +0000

    vommit 13

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     return (
         <div>
             <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light shadow ">
-                <Link to='/'><img className="logo ml-3" src='../public/images/g-shoe-new-logo.jpg' alt='logo' /></Link>
+                <Link to='/'><img className="logo ml-3" src='./images/g-shoe-new-logo.jpg' alt='logo' /></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
