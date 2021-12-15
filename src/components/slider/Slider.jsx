cf7305ba0ce13692aa8785ca94b28fec4b940fc0
Your Name <you@example.com>
2021-12-15 23:13:20 +0000

    commit 31

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -2,13 +2,12 @@
 import React from 'react'
 import './Slider.css'
 import Button from '../button/Button'
-import shoe from '../../assets/shoe.jpg'
+
 
 const Slider = () => {
     return (
         <div className="main-image">
             <div class="img-content">
-                 <div styles={{backgroundImage:`url(${shoe})`}}></div> 
                 <p>THINKING OF BUYING A SHOES</p>
                 <h2>
                     Trending <br /> Shoes
