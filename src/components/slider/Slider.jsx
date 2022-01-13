a286126a4020798252f474b0c994d4b799f9d8e2
Your Name <you@example.com>
2022-01-13 12:16:56 +0000

    commit Slider.jsx

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -7,6 +7,11 @@ const Slider = () => {
     return (
         <div className="main-image">
             <div className="img-content">
+            <img
+                 className="logo ml-3"
+                 src="./images/g-shoe-new-logo.jpg"
+                 alt="logo"
+               />
                 <p>THINKING OF BUYING SHOES</p>
                 <h2>
                     Trending Shoes
